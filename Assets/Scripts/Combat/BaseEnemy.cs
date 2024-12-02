52a057b7ba8ba9c5555b395cb354d83139ad89b7
MegaInfernape20 <andersonadrielanthony@gmail.com>
2024-12-02 17:41:30 -0500

    battle WIP

---
+++ Assets/Scripts/Combat/BaseEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseEnemy
+{
+    public string name;
+
+    public enum Type
+    {
+        FIRE,
+        COLD,
+        LIGHTNING
+    }
+
+    public Type EnemyType;
+
+    public float baseHP;
+    public float curHP;
+
+    public float baseMP;
+    public float curMP;
+
+    public float baseATK;
+    public float curATK;
+    public float baseDEF;
+    public float curDEF;
+}
---
+++ Assets/Scripts/Combat/BasePlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BasePlayer
+{
+    public string name;
+
+    public float baseHP;
+    public float curHP;
+
+    public float baseMP;
+    public float curMP;
+
+    
+}
